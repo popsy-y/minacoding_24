276900aa4b26bd7fc289823e28e9b7206a265395
Gen Sakurai <g.sakurai123@gmail.com>
2024-06-06 01:37:41 +0900

    update: day 05

--- src/daily/05.ts
+++ src/daily/05.ts
@@ -1,28 +1,70 @@
 import { info } from "../types"
 import { dailySketch } from "../interfaces"
-import p5 from "p5"
+import p5, { Color } from "p5"
 
 export const sketch:dailySketch = {
     info: {
-        day: 0,
-        theme: "some theme"
+        day: 5,
+        theme: "Many"
     },
-    fps: 15,
-    init: () => {},
+    fps: 1,
+    init: (p: p5) => {
+        p.noStroke()
+    },
     draw: (p: p5) => draw(p)
 }
 
 export const draw = (p: p5) => {
-    p.background(255, 255, 255, 100)
+    p.background(p.color("#1c2433"))
+
+    const buf = p.createGraphics(p.width, p.height)
     
-    for (let i = 0; i < Math.floor(Math.random() * 10); i++) {
-        p.rect(
-            p.random(-10, p.width + 10),
-            p.random(-10, p.height + 10),
-            p.random(p.width / 10, p.width / 3),
-            p.random(p.height / 10, p.height / 3)
+    for (let j = 0; j < 20; j++){
+        p.push()
+        p.translate(
+            p.random(p.width),
+            p.random(p.height)
         )
+    
+        for (let i = 0; i < 10; i++) {
+            drawWire(p, buf)
+        }
+        p.pop()
     }
 
-    p.text("not yet comming...", 10, 10)
+    const orig = p.createGraphics(p.width, p.height)
+    orig.image(buf, 0, 0)
+    
+    p.image(buf, 0, 0)
+    buf.filter(p.BLUR, 5)
+    // p.filter(p.BLUR, 3)
+    p.image(orig, 0, 0)
+}
+
+const drawWire = (p: p5, buf: p5.Graphics) => {
+    const vtxs: p5.Vector[] = []
+
+    const step = 20
+    const unitAng = p.PI / 4
+
+    let pos = new p5.Vector(0, 0)
+
+    for (let i = 0; i < p.floor(p.random(4, 30)); i++) {
+        const ang = unitAng * p.floor(p.random(p.TWO_PI / unitAng))
+        pos.x += step * p.cos(ang)
+        pos.y += step * p.sin(ang)
+
+        vtxs.push(pos.copy())
+    }
+
+    p.stroke(p.random() < .5 ? p.color("#31e1f5") : p.color("#f59631"))
+    p.noFill()
+    p.beginShape()
+
+    for (const v of vtxs) {
+        p.vertex(v.x, v.y)
+    }
+
+    p.endShape()
+    p.noStroke()
 }
